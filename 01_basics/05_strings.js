215884e67181aa02172ff7643cb3078b5405dba6
AyushAg310 <ayushagarwalop3@gmail.com>
2025-08-24 06:10:35 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Ayush"
+const surname = "Agarwal"
+
+// console.log(name + surname + " Value")
+console.log(`My name is ${name} and my surname is ${surname}`)
+
+const gamename = new String('AyushAg')
+console.log(gamename[0]);
