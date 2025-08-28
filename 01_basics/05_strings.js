1fbbf6042c3134709a42fd8ce30a751419bc7e79
AyushAg310 <ayushagarwalop3@gmail.com>
2025-08-28 11:02:24 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,4 +5,16 @@ const surname = "Agarwal"
 console.log(`My name is ${name} and my surname is ${surname}`)
 
 const gamename = new String('AyushAg')
+
 console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(3))
+console.log(gamename.indexOf('u'))
+console.log(gamename.substring(0,5))
+console.log(gamename.slice(-7,7))
+
+const newString = "   ayush   "
+console.log(newString)
+console.log(newString.trim())
