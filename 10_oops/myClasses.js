468b6f2df059e1d7f5dd23fc3aefbfd754126201
AyushAg310 <ayushagarwalop3@gmail.com>
2026-01-08 20:05:07 +0000

    classes in js

---
+++ 10_oops/myClasses.js
@@ -0,0 +1,21 @@
+
+class User {
+    constructor(username,email,password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`;
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const user = new User("ayush","ayush@gmail.com","123");
+console.log(user.encryptPassword());
+console.log(user.changeUsername());
+
+
